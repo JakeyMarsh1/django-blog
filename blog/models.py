b50861a8bc7d27d2e4b13e921b0f1bb32257074f
Jakeymarsh1 <Jake.o.marsh@hotmail.co.uk>
2025-09-17 12:59:25 +0100

    updated CRUD functionality on comments

--- blog/models.py
+++ blog/models.py
@@ -32,7 +32,7 @@ class Comment(models.Model):
         User, on_delete=models.CASCADE, related_name="commenter"
     )
     body = models.TextField()
-    approved_on = models.BooleanField(default=False)
+    approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
 
     class Meta:
